2a6120dd023a74603345e68a481d5bc59f0f1c3b
Miguel Celen <miguel.celen@gmail.com>
2025-12-26 23:40:15 +0100

    add config, autoloader & database connection

---
+++ classes/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+class Database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (self::$instance === null) {
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
+            self::$instance = new PDO($dsn, DB_USER, DB_PASS);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "webshop");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+require_once "autoload.php";
