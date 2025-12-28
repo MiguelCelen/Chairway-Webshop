74f49a81c4a52a21e49ac5d52e7fdede38ef3577
Miguel Celen <miguel.celen@gmail.com>
2025-12-28 23:19:32 +0100

    add main text, footer and images to the home page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $user = User::currentUser();
         <title>CHAIRWAY/Home</title>
         <link rel="stylesheet" href="public/css/style.css">
         <script defer src="public/js/script.js"></script>
-        
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
@@ -77,5 +77,83 @@ $user = User::currentUser();
               <img src="Assets/Images/dillon-mangum-9489sFfgk4c-unsplash.jpg" alt="Hero Image" style="width: 100%; height: auto;">
             </div>
       </header>
+        
+        <div class="container">
+        <main>
+            <div class="row g-2" style="margin-top: 5rem;">
+              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+                <img src="Assets/Icons/Puzzle.svg" alt="About us" style="width: 25%; height: auto;">
+                <h6>Quality and Durability</h6>
+                <p>Offer high-quality, durable furniture made from premium materials. Skilled craftsmanship and strict quality checks build trust and ensure long-lasting satisfaction.</p>
+              </div>
+              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+                <img src="Assets/Icons/Swatches_Palette.svg" alt="About us" style="width: 25%; height: auto;">
+                <h6>Innovative and Customizable Designs</h6>
+                <p>Provide unique, stylish, and functional designs with options for customization, allowing customers to personalize furniture to fit their tastes and needs.</p>
+              </div>
+              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+                <img src="Assets/Icons/Leaf.svg" alt="About us" style="width: 25%; height: auto;">
+                <h6>Sustainability</h6>
+                <p>Use eco-friendly materials and sustainable manufacturing processes. Offering recyclable or reusable furniture appeals to environmentally conscious consumers.</p>
+              </div>
+              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+                <img src="Assets/Icons/Chat_Conversation_Circle.svg" alt="About us" style="width: 25%; height: auto;">
+                <h6>Exceptional Customer Service</h6>
+                <p>Prioritize excellent customer service with smooth online shopping, reliable delivery, easy returns, and helpful post-purchase support, ensuring customer loyalty and satisfaction.</p>
+              </div>
+            </div>
+
+            <div class="row g-2" style="margin-top: 5rem;">
+              <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                <img src="Assets/Images/pawel-chu-ULh0i2txBCY-unsplash.jpg" alt="About us" style="width: 80%; height: auto; border-radius: 10px">
+              </div>
+              <div class="col-lg-6">
+                <h2>about us</h2>
+                <p>
+                  At Chairway, we understand that furniture is an extension of your personality and lifestyle. That’s why our collections are thoughtfully designed to offer a blend of timeless classics and contemporary trends, ensuring there’s something for everyone. From cozy chairs to complete room solutions, we aim to inspire and elevate your living spaces.
+
+                  Sustainability is at the heart of everything we do. We take pride in using responsibly sourced materials and eco-conscious manufacturing practices, creating pieces that are not only beautiful but also kind to the planet. <br><br>
+                  
+                  Our mission is to make your furniture journey seamless and enjoyable. With customizable options, reliable delivery, and a dedicated customer support team, we’re here to turn your dream spaces into reality.
+                  
+                  Discover the art of exceptional living with Chairway—your partner in crafting spaces you’ll love for years to come.
+                </p>
+                <a href="...">
+                  <button type="button" class="btn btn-dark">Dummy</button>
+                </a>
+              </div>
+            </div>
+
+            <div id="top-selling-container" class="row" style="margin-top: 5rem;">
+            </div>
+
+            <div class="row g-2" style="margin-top: 5rem;">
+              <div class="col-lg-4 d-flex justify-content-center align-items-center">
+                <img src="Assets/Images/mail.png" alt="Get in contact" style="width: 50%; height: auto;">
+              </div>
+              <div class="col-lg-8">
+                <h2>Let's get in contact</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique leo non erat semper, eu suscipit erat scelerisque.
+                  Suspendisse vitae fringilla turpis. Integer hendrerit sollicitudin tellus. Cras tempus eget nibh ut lacinia. 
+                  Morbi dignissim aliquet arcu, vel dapibus mauris egestas a.
+                  Aenean diam nisl, gravida at neque nec, imperdiet pretium mi. Mauris et pharetra sem.
+                </p>
+                <a href="pages/ContactUs.php">
+                  <button type="button" class="btn btn-dark">Get in contact</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <footer>
+          <div class="row g-2"></div>
+          <div class="text-center p-3" style="margin-top: 5rem;">
+            © 2025 Copyright: Chairway
+          </div>
+        </footer>
+
+        <script src="topSellingItems.js"></script>
     </body>
 </html>
