27d4ee36b7e48046a0f501626a575c5a285ccd51
Miguel Celen <miguel.celen@gmail.com>
2026-01-01 18:44:42 +0100

    modify code for responsiveness of home page

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $user = User::currentUser();
     <html lang="nl">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>CHAIRWAY/Home</title>
         <link rel="stylesheet" href="public/css/style.css">
         <script defer src="public/js/script.js"></script>
-
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </head>
@@ -81,22 +81,22 @@ $user = User::currentUser();
         <div class="container">
         <main>
             <div class="row g-2" style="margin-top: 5rem;">
-              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+              <div class="col-12 col-sm-6 col-lg-3 d-flex flex-column align-items-center text-center">
                 <img src="Assets/Icons/Puzzle.svg" alt="About us" style="width: 25%; height: auto;">
                 <h6>Quality and Durability</h6>
                 <p>Offer high-quality, durable furniture made from premium materials. Skilled craftsmanship and strict quality checks build trust and ensure long-lasting satisfaction.</p>
               </div>
-              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+              <div class="col-12 col-sm-6 col-lg-3 d-flex flex-column align-items-center text-center">
                 <img src="Assets/Icons/Swatches_Palette.svg" alt="About us" style="width: 25%; height: auto;">
                 <h6>Innovative and Customizable Designs</h6>
                 <p>Provide unique, stylish, and functional designs with options for customization, allowing customers to personalize furniture to fit their tastes and needs.</p>
               </div>
-              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+              <div class="col-12 col-sm-6 col-lg-3 d-flex flex-column align-items-center text-center">
                 <img src="Assets/Icons/Leaf.svg" alt="About us" style="width: 25%; height: auto;">
                 <h6>Sustainability</h6>
                 <p>Use eco-friendly materials and sustainable manufacturing processes. Offering recyclable or reusable furniture appeals to environmentally conscious consumers.</p>
               </div>
-              <div class="col-lg-3 d-flex flex-column align-items-center text-center">
+              <div class="col-12 col-sm-6 col-lg-3 d-flex flex-column align-items-center text-center">
                 <img src="Assets/Icons/Chat_Conversation_Circle.svg" alt="About us" style="width: 25%; height: auto;">
                 <h6>Exceptional Customer Service</h6>
                 <p>Prioritize excellent customer service with smooth online shopping, reliable delivery, easy returns, and helpful post-purchase support, ensuring customer loyalty and satisfaction.</p>
@@ -104,11 +104,11 @@ $user = User::currentUser();
             </div>
 
             <div class="row g-2" style="margin-top: 5rem;">
-              <div class="col-lg-6 d-flex justify-content-center align-items-center">
+              <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center mb-3 mb-lg-0">
                 <img src="Assets/Images/pawel-chu-ULh0i2txBCY-unsplash.jpg" alt="About us" style="width: 80%; height: auto; border-radius: 10px">
               </div>
-              <div class="col-lg-6">
-                <h2>about us</h2>
+              <div class="col-12 col-lg-6">
+                <h2>About us</h2>
                 <p>
                   At Chairway, we understand that furniture is an extension of your personality and lifestyle. That’s why our collections are thoughtfully designed to offer a blend of timeless classics and contemporary trends, ensuring there’s something for everyone. From cozy chairs to complete room solutions, we aim to inspire and elevate your living spaces.
 
@@ -128,10 +128,10 @@ $user = User::currentUser();
             </div>
 
             <div class="row g-2" style="margin-top: 5rem;">
-              <div class="col-lg-4 d-flex justify-content-center align-items-center">
-                <img src="Assets/Images/mail.png" alt="Get in contact" style="width: 50%; height: auto;">
+              <div class="col-12 col-lg-4 d-flex justify-content-center align-items-center mb-3 mb-lg-0">
+                <img src="Assets/Images/pawel-chu-ULh0i2txBCY-unsplash.jpg" class="img-fluid rounded" alt="Get in contact">
               </div>
-              <div class="col-lg-8">
+              <div class="col-12 col-lg-8">
                 <h2>Let's get in contact</h2>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique leo non erat semper, eu suscipit erat scelerisque.
