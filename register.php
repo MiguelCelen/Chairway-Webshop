9a688a3d4342d33dae67dcfcf4ac592ddafb85ed
Miguel Celen <miguel.celen@gmail.com>
2026-01-04 20:29:12 +0100

    modify code for responsiveness of register page

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Registreren - Chairway</title>
     <link rel="stylesheet" href="public/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -10,16 +11,16 @@
 </head>
 <body class="bg-light">
 <div class="container-fluid min-vh-100 d-flex align-items-center">
-    <div class="row flex-grow-1">
-        <div class="col-md-5 d-flex align-items-center justify-content-center">
-            <div class="w-75">
+    <div class="row flex-grow-1 w-100 justify-content-center">
+        <div class="col-12 col-md-8 col-lg-5 d-flex align-items-center justify-content-center">
+            <div class="w-90 w-md-75 px-3 px-md-0 mx-auto" style="max-width: 480px;">
                 <a href="index.php"><img src="Assets/Icons/back-button.png" alt="black back button" style="width:25px;height:25px;"></a>
                 <h1 class="mb-3">Maak een account aan 🪑</h1>
                 <p class="text-muted mb-4">
                     Registreer je bij <strong>Chairway</strong> om te starten met shoppen.
                 </p>
 
-                <form action="handlers/register.php" method="POST" class="needs-validation" novalidate>
+                <form action="handlers/register.php" method="POST" class="needs-validation w-100" novalidate>
                     <div class="mb-3">
                         <label for="name" class="form-label">Naam</label>
                         <input type="text" class="form-control" id="name" name="name" required>
@@ -35,7 +36,7 @@
                         <input type="password" class="form-control" id="password" name="password" required>
                         <div class="invalid-feedback">Vul een wachtwoord in.</div>
                     </div>
-                    <button type="submit" class="btn btn-dark w-100">Account aanmaken</button>
+                    <button type="submit" class="btn btn-dark w-100 py-2">Account aanmaken</button>
                 </form>
                 
                 <p class="mt-3 small text-muted">
@@ -44,7 +45,7 @@
                 </p>
             </div>
         </div>
-        <div class="col-md-6 d-none d-md-block p-0">
+        <div class="col-lg-6 d-none d-lg-block p-0">
             <div class="h-100"
                  style="
                     background-image: url('Assets/Images/pawel-chu-ULh0i2txBCY-unsplash.jpg');
