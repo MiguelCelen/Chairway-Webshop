25c667a782c2ef0ece4da293c0db260abf33da44
Miguel Celen <miguel.celen@gmail.com>
2026-01-05 15:37:02 +0100

    modify register handler

--- handlers/register.php
+++ handlers/register.php
@@ -2,7 +2,8 @@
 require_once("../config.php");
 try {
     User::register($_POST["email"], $_POST["password"], $_POST["name"]);
-    echo "Account aangemaakt! Je kan nu inloggen.";
+    header("Location: ../index.php");
+    exit;
 } catch (Exception $e) {
     echo "Registratie fout: " . $e->getMessage();
 }
